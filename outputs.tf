e4a193108d0f42b0cf83cdd7bc4b214589167d31
Yuliana <magician_seeker@yahoo.com>
2021-05-18 15:20:06 +0700

    Add monitor id as output so it could be referenced in datadog_service_level_objective (#15)
    * Add monitor id as output so it could be referenced in datadog_service_level_objective https://github.com/DataDog/terraform-provider-datadog/issues/667
    
    * fix variable name

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "name" {
   value       = "${join(",", datadog_monitor.template.*.name)}"
   description = "The name of datadog monitor"
 }
+
+output "monitor_id" {
+  value       = "${datadog_monitor.template.*.id}"
+  description = "The id(s) of datadog monitor created"
+}
