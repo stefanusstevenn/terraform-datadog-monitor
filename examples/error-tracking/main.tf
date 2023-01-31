230abbcee733f0e4816efc288371a49eaa14204f
stefanusstevenn <stevensteven@traveloka.com>
2023-01-31 21:58:19 +0700

    enhance error-tracking alert example

--- examples/error-tracking/main.tf
+++ examples/error-tracking/main.tf
@@ -6,6 +6,19 @@ locals {
   thresholds = {
     critical = 1
   }
+
+  condition           = "count"
+  evaluate_query_time = "1d"
+
+  name = {
+    "count"     = "${local.product_domain} - ${local.environment} - Error \"{{ log.attributes.[error.kind] }}\" occurred in ${local.service}",
+    "new issue" = "${local.product_domain} - ${local.environment} - Error \"{{ log.attributes.[error.kind] }}\" first time occurred in ${local.service}"
+  }
+
+  query = {
+    "count"     = "error-tracking-logs(\"service:${local.service}\").index(\"*\").rollup(\"count\").by(\"issue.id\").last(\"${local.evaluation_query}\") >= 1",
+    "new issue" = "error-tracking-logs(\"service:${local.service} issue.age:<=300000\").index(\"*\").rollup(\"count\").by(\"issue.id\").last(\"5m\") >= 1"
+  }
 }
 
 module "datadog_log_error_tracking" {
@@ -14,13 +27,13 @@ module "datadog_log_error_tracking" {
   service        = local.service
   environment    = local.environment
 
-  name             = "${local.product_domain} - ${local.environment} - Error {{ log.attributes.[error.kind] }} occurred in ${local.service}"
+  name             = local.name[local.condition]
   type             = "error-tracking alert"
-  query            = "error-tracking-logs(\"service:${local.service} issue.age:<=86400000\").index(\"*\").rollup(\"count\").by(\"issue.id\").last(\"1d\") >= 1"
+  query            = local.query[local.condition]
   
   thresholds       = local.thresholds
 
-  alert_recipients   = ["slack-traveloka-backend-infra-testing"]
+  alert_recipients   = ["slack-traveloka-datadog-error-tracking-test"]
   include_tags       = false
 }
 
